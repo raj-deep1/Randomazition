069ded099abdfb691f6cb6458f97d78db019ee20
raj-deep1 <ea.sup.sar.101602@gmail.com>
2026-06-13 12:07:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Randomazition/', // Aapke repository ka exact naam yahan aayega
+})
+
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
